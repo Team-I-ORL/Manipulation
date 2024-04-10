cddc8f64975a0f7f9bc23c55302bd6ed7d1f38cd
gauravsethia08 <gaurav.sethia08@gmail.com>
2024-04-10 03:55:47 -0400

    Bucket launch file added

--- fetch_moveit_config_2/launch/demo.launch.py
+++ fetch_moveit_config_2/launch/demo.launch.py
@@ -3,5 +3,10 @@ from moveit_configs_utils.launches import generate_demo_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("fetch", package_name="fetch_moveit_config_2").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("fetch", package_name="fetch_moveit_config_2").robot_description(
+            file_path="config/fetch.urdf.xacro",
+            mappings={
+                "ros2_control_hardware_type": 'isaac'
+            },
+        ).to_moveit_configs()
     return generate_demo_launch(moveit_config)
--- fetch_moveit_config_2/launch/move_group.launch.py
+++ fetch_moveit_config_2/launch/move_group.launch.py
@@ -1,7 +1,33 @@
 from moveit_configs_utils import MoveItConfigsBuilder
 from moveit_configs_utils.launches import generate_move_group_launch
+from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("fetch", package_name="fetch_moveit_config_2").to_moveit_configs()
+
+    moveit_config = (
+        MoveItConfigsBuilder("fetch", package_name="fetch_moveit_config_2")
+        .robot_description(
+            file_path="config/fetch.urdf.xacro",
+            # mappings={
+            #     "ros2_control_hardware_type": LaunchConfiguration(
+            #         "ros2_control_hardware_type"
+            #     )
+            # },
+        )
+        .robot_description_semantic(file_path="config/fetch.srdf")
+        .planning_scene_monitor(
+           publish_robot_description=True, publish_robot_description_semantic=True,
+           publish_geometry_updates= True, publish_state_updates= True, 
+           publish_transforms_updates= True
+        )
+        # #.moveit_cpp(file_path="/home/siddharth/ws_moveit/src/Manipulation/fetch_moveit_config/config/motion_planning_python_api_tutorial.yaml")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner", "stomp"]
+        )
+        .to_moveit_configs()
+    )
+
+    # moveit_config = MoveItConfigsBuilder("fetch", package_name="fetch_moveit_config_2").to_moveit_configs()
     return generate_move_group_launch(moveit_config)
